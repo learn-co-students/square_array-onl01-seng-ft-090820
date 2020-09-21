7e0f49b697ee63fb865953003c51a38230232023
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 14:39:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,3 +5,12 @@ def square_array(numbers)
   end
   return new_numbers
 end
+
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+my_arr = [1, 2]
+p square_array(my_arr)

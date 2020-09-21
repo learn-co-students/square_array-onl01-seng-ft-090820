e7739deb80544031dce513b4f6804a320c9e4ce2
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 14:31:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_numbers = [1, 2, 3]
+  numbers.each do |num|
+  numbers.each { |i| new_numbers << i ** 2 }
+  end
+  return new_numbers
 end
